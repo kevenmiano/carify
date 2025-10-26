70ef37253c60b2b1348a241c4c21fb9cc26daf75
kevenmiano <k_l.s@outlook.com>
2025-10-26 00:45:55 -0200

    feat: add flask setup

---
+++ carify/app.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+from flask import Flask
+from flask_cors import CORS
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+cors = CORS(app)
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=os.getenv('PORT'), load_dotenv=True)
